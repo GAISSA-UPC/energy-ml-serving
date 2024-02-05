044c0456ea4bf0e89fb2b40208c052f976c37c65
fjdurlop <fjdurlop0@gmail.com>
2024-02-05 13:23:50 +0000

    updating config files

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo 'Starting server using uvicorn, API for code LMs'
-uvicorn app.api_code:app  --host 0.0.0.0 --port 8000  --reload  --reload-dir . --reload-dir app 
+uvicorn app.api_code:app  --host 0.0.0.0 --port 8000  --reload   --reload-dir app 
--- testing/run_inferences.sh
+++ testing/run_inferences.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-python3 testing/main.py -i ov -r 1 | tee -a results/out_ov.log;
-python3 testing/main.py -i ov -r 10 | tee -a results/out_ov.log;
-python3 testing/main.py -i onnx -r 1 | tee -a results/out_onnx.log;
-python3 testing/main.py -i onnx -r 10 | tee -a results/out_onnx.log;
-python3 testing/main.py -i torchscript -r 1 | tee -a results/out_torchscript.log;
-python3 testing/main.py -i torchscript -r 10 | tee -a results/out_torchscript.log;
-python3 testing/main.py -i torch -r 1 | tee -a results/out_torch.log;
-python3 testing/main.py -i torch -r 10 | tee -a results/out_torch.log;
+REPS=1
+python3 testing/main.py -i ov -r $REPS | tee -a results/out_ov.log;
+python3 testing/main.py -i onnx -r $REPS | tee -a results/out_onnx.log;
+python3 testing/main.py -i torchscript -r $REPS | tee -a results/out_torchscript.log;
+python3 testing/main.py -i torch -r $REPS | tee -a results/out_torch.log;
