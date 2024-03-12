c71039a8825d314de2c3c5a0e817ee707a2884b3
fjdurlop <fjdurlop0@gmail.com>
2024-03-12 23:18:42 +0000

    experiments: bash script update

--- runall.sh
+++ runall.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# nohup python3 start_server.py >  results/nohup.out 2>&1 &
+# nohup ./runall.sh > results/runall.out 2>&1 &
+# nohup ./runall.sh ov > results/runall.out 2>&1 &
+# nohup ./runall.sh ov > results/runall.out 2>&1 &
+
 # set true if need to install from scratch
 INSTALL=false
 # set true to run experiments with all runtime engines
-ALL_RUNTIME_ENGINES=false
+ALL_RUNTIME_ENGINES=true
+# set true if server needs to be started from this script
+START_SERVER=true
+
 SECONDS=0
 WAIT_BETWEEN_RUNTIME=10 # Wait time between running experiments with each runtime engine
 REPS=1 # Number of repetitions for each runtime engine experiment
 
-START_SERVER=true # set true if server needs to be started from this script
-
-SERVER_LOG="results/output_050324_04_$1.log" #change
-
+SERVER_LOG="results/output_120324_01_$1.log" #change
 python3=/home/usuaris/fduran/Python-3.8.4/python
 python3=python3 # comment if rdlab
 
@@ -56,10 +61,10 @@ if [ $START_SERVER = true ]; then
     
     #uvicorn app.api_code:app  --host 0.0.0.0 --port 8000   > output_01.log 2>&1 & #  --reload --reload-dir app
     $python3 start_server.py >  $SERVER_LOG 2>&1 &
+    
 fi
 
 #nvidia-smi -i 1 --query-gpu=timestamp,gpu_name,utilization.gpu,utilization.memory,memory.total,memory.used,power.draw,power.max_limit,temperature.gpu --format=csv -l 1 -f output_gpu
-
 
 if [ $ALL_RUNTIME_ENGINES = true ]; then
     print "USING ALL RUNTIME ENGINES"
@@ -68,8 +73,13 @@ if [ $ALL_RUNTIME_ENGINES = true ]; then
     print "| Running experiments, RUNTIME ENGINE -> "TORCH" |"
     print "---------------------------------------------------------------"
     start_time=$(date +%s.%N)
-    $python3 testing/main.py -i torch -r $REPS | tee -a results/out_torch.log;
-    
+    #$python3 testing/main.py -i torch -r $REPS | tee -a results/out_torch.log;
+    runtime="torch"
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5-base' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5p-220' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codeparrot-small' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'pythia-410m' | tee -a results/out_$runtime.log;
+
     # Calculate the elapsed time
     end_time=$(date +%s.%N)
     elapsed_time=$(echo "$end_time - $start_time" | bc)
@@ -85,8 +95,12 @@ if [ $ALL_RUNTIME_ENGINES = true ]; then
     print "| Running experiments, RUNTIME ENGINE -> "ONNX" |"
     print "---------------------------------------------------------------"
     start_time=$(date +%s.%N)
-    $python3 testing/main.py -i onnx -r $REPS | tee -a results/out_onnx.log;
-
+    #$python3 testing/main.py -i onnx -r $REPS | tee -a results/out_onnx.log;
+    runtime="onnx"
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5-base' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5p-220' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codeparrot-small' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'pythia-410m' | tee -a results/out_$runtime.log;
     # Calculate the elapsed time
     end_time=$(date +%s.%N)
     elapsed_time=$(echo "$end_time - $start_time" | bc)
@@ -102,8 +116,12 @@ if [ $ALL_RUNTIME_ENGINES = true ]; then
     print "| Running experiments, RUNTIME ENGINE -> "OV" |"
     print "---------------------------------------------------------------"
     start_time=$(date +%s.%N)
-    $python3 testing/main.py -i ov -r $REPS | tee -a results/out_ov.log;
-    
+    #$python3 testing/main.py -i ov -r $REPS | tee -a results/out_ov.log;
+    runtime="ov"
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5-base' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5p-220' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codeparrot-small' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'pythia-410m' | tee -a results/out_$runtime.log;
     # Calculate the elapsed time
     end_time=$(date +%s.%N)
     elapsed_time=$(echo "$end_time - $start_time" | bc)
@@ -119,7 +137,12 @@ if [ $ALL_RUNTIME_ENGINES = true ]; then
     print "| Running experiments, RUNTIME ENGINE -> "TORCHSCRIPT" |"
     print "---------------------------------------------------------------"
     start_time=$(date +%s.%N)
-    $python3 testing/main.py -i torchscript -r $REPS | tee -a results/out_torchscript.log;
+    #$python3 testing/main.py -i torchscript -r $REPS | tee -a results/out_torchscript.log;
+    runtime="torchscript"
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5-base' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codet5p-220' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'codeparrot-small' | tee -a results/out_$runtime.log;
+    $python3 testing/main.py -i $runtime -r $REPS -m 'pythia-410m' | tee -a results/out_$runtime.log;
 
     # Calculate the elapsed time
     end_time=$(date +%s.%N)
@@ -133,7 +156,12 @@ else
     print "| Running experiments, RUNTIME ENGINE -> "$1" |"
     print "---------------------------------------------------------------"
     start_time=$(date +%s.%N)
-    $python3 testing/main.py -i $1 -r $REPS | tee -a results/out_$1.log;
+    
+    $python3 testing/main.py -i $1 -r $REPS -m 'codeparrot-small' | tee -a results/out_$1.log;
+    $python3 testing/main.py -i $1 -r $REPS -m 'pythia-410m' | tee -a results/out_$1.log;
+    $python3 testing/main.py -i $1 -r $REPS -m 'codet5p-220' | tee -a results/out_$1.log;
+    $python3 testing/main.py -i $1 -r $REPS -m 'codet5-base' | tee -a results/out_$1.log;
+
 
     # Calculate the elapsed time
     end_time=$(date +%s.%N)
