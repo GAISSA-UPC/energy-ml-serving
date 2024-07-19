f5b8a0e483374d5248749c083145f09f3d1e8e87
fjdurlop <fjdurlop0@gmail.com>
2024-07-19 13:10:28 +0200

    updating script

--- scripts/clean_processes.sh
+++ scripts/clean_processes.sh
@@ -4,7 +4,9 @@ pkill -f repeat.sh
 pkill -f runall_update.sh
 pkill -f python3
 pkill -f '/home/fjdur/EnergiBridge/target/release/energibridge'
+pkill -f nvidia-smi
 
 ps aux | grep runall
 ps aux | grep energibridge
 ps aux | grep python
+ps aux | grep nvidia-smi
